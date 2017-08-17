aa38a1b647df9fa792434fdf01bcb24e55e9af44
Dean Holt <d.holt@mail.com>
2017-08-17 21:37:25 +1000

    Update kodeweave.js

--- editor/kodeweave.js
+++ editor/kodeweave.js
@@ -1,7 +1,7 @@
-// If domain is HTTP
+/*// If domain is HTTP
 var site = window.location;
 site = site.toString();
-/*
+
 if (site.substring(0, 7) === "http://") {
   window.location.href = "https://" + site.substring(7, site.length);
 }
